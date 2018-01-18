e06495478e77619eaf738612c6a7b4705e09e7a6
Raamakrishnan <raamakrishnan1997@gmail.com>
2018-01-18 12:06:01 +0530

    added branch, load, store. fixed few bugs in ports

--- src/execute.v
+++ src/execute.v
@@ -17,7 +17,7 @@ module execute(
     input wire exception_in_valid,
     input wire pipeline_in_valid,
     input wire [4:0] opcode_in,
-    input wire [2:0] funct,
+    input wire [2:0] funct_in,
     input wire variant,
     input wire [`REG_DATA_SIZE : 0] op1,
     input wire [`REG_DATA_SIZE : 0] op2,
@@ -29,15 +29,17 @@ module execute(
     output reg [`ADDR_SIZE : 0] PC_out,
     output reg [`INSTR_SIZE : 0] instr_out,
     `endif
-    output reg opcode_out,
+    output reg [4:0] opcode_out,
+    output reg [2:0] funct_out,
     output reg nop_instr_out,
     output reg [`EX_WIDTH : 0] exception_out,
     output reg exception_out_valid,
     output reg pipeline_out_valid,
-    output reg result,
-    output reg rd_addr_out,
+    output reg [`REG_DATA_SIZE : 0] result,
+    output reg [`ADDR_SIZE : 0] store_addr,
+    output reg [`REG_ADDR_SIZE : 0] rd_addr_out,
     output reg flush_out,
-    output reg flush_addr,
+    output reg [`ADDR_SIZE : 0] flush_addr,
 
     input wire stall,
     input wire flush_in
@@ -50,31 +52,31 @@ module execute(
             flush_out = 0;
             if(exception_in_valid == 0 && nop_instr_in == 0) begin
                 if(opcode_in == `OP_IMM_ARITH || opcode_in == `OP_ARITH) begin
-                    if(funct == `F3_ADD_SUB) begin
+                    if(funct_in == `F3_ADD_SUB) begin
                         if(variant == 0)        //ADD
                             result = op1 + op2;
                         else                    //SUB
                             result = op1 - op2;
                     end
-                    else if(funct == `F3_SLT_SLTI) begin
+                    else if(funct_in == `F3_SLT_SLTI) begin
                         result = (op1 < op2);
                     end
-                    else if(funct == `F3_SLTU_SLTIU) begin
+                    else if(funct_in == `F3_SLTU_SLTIU) begin
                         result = (op1 < op2);
                     end
-                    else if(funct == `F3_XOR_XORI) begin
+                    else if(funct_in == `F3_XOR_XORI) begin
                         result = (op1 ^ op2);
                     end
-                    else if(funct == `F3_OR_ORI) begin
+                    else if(funct_in == `F3_OR_ORI) begin
                         result = (op1 | op2);
                     end
-                    else if(funct == `F3_AND_ANDI) begin
+                    else if(funct_in == `F3_AND_ANDI) begin
                         result = (op1 & op2);
                     end
-                    else if(funct == `F3_SLL_SLLI) begin
+                    else if(funct_in == `F3_SLL_SLLI) begin
                         result = op1 << op2[4:0];
                     end
-                    else if(funct == `F3_SR_SRI) begin
+                    else if(funct_in == `F3_SR_SRI) begin
                         if(variant == 0)        //SRL
                             result = op1 >> op2[4:0];
                         else
@@ -91,6 +93,25 @@ module execute(
                     flush_out = 1;
                     flush_addr = (op1 + op2) & 1'b0;
                 end
+                else if(opcode_in == `OP_BRANCH) begin
+                    flush_addr = PC_in + offset;
+                    case (funct_in)
+                        `F3_BEQ:  flush_out = (op1 == op2);
+                        `F3_BNE:  flush_out = (op1 != op2);
+                        `F3_BLT:  flush_out = (op1 < op2);
+                        `F3_BLTU: flush_out = ($signed(op1) < $signed(op2));
+                        `F3_BGE:  flush_out = (op1 >= op2);
+                        `F3_BGEU: flush_out = ($signed(op1) >= $signed(op2));
+                        default:  flush_out = 0;
+                    endcase
+                end
+                else if(opcode_in == `OP_LOAD) begin
+                    result = op1 + op2;
+                end
+                else if(opcode_in == `OP_STORE) begin
+                    result = op2;
+                    store_addr = op1 + offset;
+                end
             end
         end
     end
@@ -106,9 +127,7 @@ module execute(
             `endif
         end
         else if(stall) begin
-            pipeline_out_valid <= pipeline_out_valid;
-            opcode_out <= opcode_out;
-            rd_addr_out <= rd_addr_in;
+            advancePipeline;
             `ifdef SIMULATE
                 PC_out <= PC_out;
                 instr_out <= instr_out;
@@ -116,9 +135,7 @@ module execute(
             `endif
         end
         else if(pipeline_in_valid) begin
-            pipeline_out_valid <= pipeline_in_valid;
-            opcode_out <= opcode_out;
-            rd_addr_out <= rd_addr_in;
+            advancePipeline;
             `ifdef SIMULATE
                 PC_out <= PC_in;
                 instr_out <= instr_in;
@@ -127,5 +144,14 @@ module execute(
             `endif
         end
     end
+
+    task advancePipeline;
+        begin
+            pipeline_out_valid <= pipeline_in_valid;
+            opcode_out <= opcode_out;
+            rd_addr_out <= rd_addr_in;
+            funct_out <= flush_in;
+        end
+    endtask
 
 endmodule // execute
