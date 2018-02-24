2de8fcd031e2b00f4adab235045ed4018ddde931
Raamakrishnan <raamakrishnan1997@gmail.com>
2018-02-24 22:40:46 +0530

    storing works - unaligned and different sizes

--- tests/test1/t1.s
+++ tests/test1/t1.s
@@ -1,15 +1,13 @@
 .text
 .global _start
 _start:
-    lb x2, 3(zero)
-    lh x3, 2(zero)
-    addi x2, x0, 0x01
+    sb	zero, 0(zero)
+    lw zero, 0(zero)
     nop
     nop
     nop
     nop
-    sw	zero, 0(zero)
-    
+    nop
 .data
 	.dword 0x1020304050607080
 	.dword 0x1020304050607080
